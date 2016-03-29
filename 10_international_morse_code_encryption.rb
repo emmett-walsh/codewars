90949b6895ad47ecd3e2f79334e16c7562cc0f35
Emmett Walsh <emmett.finbarr.walsh@gmail.com>
2016-03-29 17:31:23 +0100

    Got the morse code function working

--- 10_international_morse_code_encryption.rb
+++ 10_international_morse_code_encryption.rb
@@ -1,10 +1,10 @@
-# library needed (hosted on codewars) - couldn't get output to match tests but i think that could be the tests themselves
+# library needed (hosted on codewars)
 
 def encryption(string)
   output = ""
   string.each_char do |char|
     if char == " "
-      output << " "
+      output << "  "
     else
       output << $CHAR_TO_MORSE[char] + " "
     end
